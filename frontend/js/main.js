387c30c2e45c2d33db0651f3adcbe8fafcf3f5f6
Ari Jaya <arijaya130@gmail.com>
2022-09-04 10:37:20 +0800

    Testing Error

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -3,14 +3,15 @@ $.ajaxSetup({
         function getCookie(name) {
             let cookieValue = null;
 
+
             if (document.cookie && document.cookie !== '') {
                 const cookies = document.cookie.split(';');
 
-                for (let i = 0; i < cookies.length; i+= 1) {
+                for (let i = 0; i < cookies.length; i += 1) {
                     const cookie = jQuery.trim(cookies[i]);
 
                     // Does this cookie string begin with the name we want?
-                    if (cookie.substring(0, name.length + 1) == ('$[name}=')) {
+                    if (cookie.substring(0, name.length + 1) === (`${name}=`)) {
                         cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
                         break;
                     }
@@ -30,4 +31,60 @@ $.ajaxSetup({
 $(document).on("click", ".js-toggle-modal", function(e) {
     e.preventDefault()
     $(".js-modal").toggleClass("hidden")
+})
+.on("click", ".js-submit", function(e) {
+    e.preventDefault()
+    const text = $(".js-post-text").val().trim()
+    const $btn = $(this)
+
+    if(!text.length) {
+        return false
+    }
+
+    $btn.prop("disabled", true).text("Posting!")
+    $.ajax({
+        type: 'POST',
+        url: $(".js-post-text").data("post-url"),
+        data: {
+            text: text
+        },
+        success: (dataHtml) => {
+            $(".js-modal").addClass("hidden");
+            $("#posts-container").prepend(dataHtml);
+            $btn.prop("disabled", false).text("New Post");
+            $(".js-post-text").val('')
+        },
+        error: (error) => {
+            console.warn(error)
+            $btn.prop("disabled", false).text("Error");
+        }
+    });
+})
+.on("click", ".js-follow", function(e) {
+    e.preventDefault();
+    const action = $(this).attr("data-action")
+
+    $.ajax({
+        type: 'POST',
+        url: $(this).data("url"),
+        data: {
+            action: action,
+            username: $(this).data("username"),
+        },
+        success: (data) => {
+            $(".js-follow-text").text(data.wording)
+            if(action == "follow") {
+                // Change wording to unfollow
+                console.log("DEBUG", "unfollow")
+                $(this).attr("data-action", "unfollow")
+            } else {
+                // The opposite
+                console.log("DEBUG", "follow")
+                $(this).attr("data-action", "follow")
+            }
+        },
+        error: (error) => {
+            console.warn(error)
+        }
+    });
 })
